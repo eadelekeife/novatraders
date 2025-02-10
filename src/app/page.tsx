c3ae1f90f72f3adf73c242b5ad77fccb650add7c
Adeleke Ifeoluwase <eadelekeife@gmail.com>
2025-02-10 05:37:48 +0100

    initial app setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Image from "next/image";
 
-import SS from "@/assets/images/d.jpg";
 import _1 from "@/assets/images/_1.jpg";
 import _2 from "@/assets/images/_2.jpg";
-
-import FarmImg from "@/assets/images/farm.jpg";
 
 import { BsArrowUpRightCircle } from "react-icons/bs";
 import { HiArrowTrendingUp } from "react-icons/hi2";
 import { ImArrowUpRight2 } from "react-icons/im";
-import Link from "next/link";
 import DisplayLayout from "@/components/layout";
 
 
